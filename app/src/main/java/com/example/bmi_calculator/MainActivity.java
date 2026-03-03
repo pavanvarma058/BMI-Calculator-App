6f9110dffce897ff55a81acb98a029ff1797ba64
Pavan Varma <pavanvarma058@gmail.com>
2026-03-03 16:01:11 +0000

    Added variables and created a on click listener to calculate button

--- app/src/main/java/com/example/bmi_calculator/MainActivity.java
+++ app/src/main/java/com/example/bmi_calculator/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.bmi_calculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +21,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        TextView resultView = findViewById(R.id.view_result);
+        RadioButton maleBtn = findViewById(R.id.radio_button_male);
+        RadioButton femaleBtn = findViewById(R.id.radio_button_female);
+        EditText feetText = findViewById(R.id.edit_feet);
+        EditText inchesText = findViewById(R.id.edit_inches);
+        EditText ageText = findViewById(R.id.text_age);
+        EditText weightText = findViewById(R.id.text_weight);
+        Button calculateBtn = findViewById(R.id.calculate_btn);
+
+        calculateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Wow, a btn click", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
